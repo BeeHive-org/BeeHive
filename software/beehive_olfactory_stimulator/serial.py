ab85efea46d8514208484b1b34552d6a3ed14c10
Andre Maia Chagas <andremaia.chagas@gmail.com>
2020-07-13 15:52:44 +0100

    minimum stable version of serial

--- software/beehive_olfactory_stimulator/serial.py
+++ software/beehive_olfactory_stimulator/serial.py
@@ -5,7 +5,7 @@ test for serial communication
 here we just create a serial object that will take in data to be parsed and sent over the serial port
 '''
 print("imported")
-class SerialTest:
+class Serial:
     ''' simple class to constantly read and write serial data
         for test purposes only'''
     def __init__(self,baudRate=115200):
@@ -28,18 +28,21 @@ class SerialTest:
         self.uart.write(data2Write)
         return
     
-    def readData(self,amount2Read=0):
+    def readData(self):#amount2Read=0):
         '''use poll method to see how many characters are available for reading, 
         read them, and return it
         OBS: need to learn what is the UART buffer size and implement controls on the PC side'''
         poll = uselect.poll()
-        poll.register(uart, uselect.POLLIN)
+        poll.register(self.uart, uselect.POLLIN)
         
         #timeout=-1 polls the port indefintely. poll.poll returns  a list of tuples that need to be worked on 
-        data = poll.poll(timeout=-1)
+        amount2read = poll.poll()
+        amount2read = amount2read[0][1]+4 #+4 reads the trailing "/n/r" at the end of each line
+
+        data = self.uart.read(amount2read)
         
-        print(data)
+        #print(data)
         #add if statement to make use of poll
         #readData = self.uart.read(amount2Read)
         #print(readData)
-        #return readData
+        return data
