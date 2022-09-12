3add92b503693565671d39b7e4d655fe61b3dbdb
Andre Maia Chagas <andremaia.chagas@gmail.com>
2022-09-12 17:19:50 +0100

    Update example1.py
    example to use solenoid driver

--- software/solenoid_driver/example1.py
+++ software/solenoid_driver/example1.py
@@ -5,26 +5,27 @@ sole1 = Pin(2,Pin.OUT,drive=Pin.DRIVE_3)
 sole2 = Pin(15,Pin.OUT,drive=Pin.DRIVE_3)
 sole3 = Pin(16,Pin.OUT,drive=Pin.DRIVE_3)
 sole4 = Pin(17,Pin.OUT,drive=Pin.DRIVE_3)
-
-for i in range(10):
+timeOn=1000
+timOff=500
+for i in range(5):
     print("loop: "+str(i+1))
     sole1.on()
-    time.sleep_ms(500)
+    time.sleep_ms(timeOn)
     sole1.off()
-    time.sleep_ms(100)
+    time.sleep_ms(timOff)
     
     sole2.on()
-    time.sleep_ms(500)
+    time.sleep_ms(timeOn)
     sole2.off()
-    time.sleep_ms(100)
+    time.sleep_ms(timeOff)
     
     sole3.on()
-    time.sleep_ms(500)
+    time.sleep_ms(timeOn)
     sole3.off()
-    time.sleep_ms(100)
+    time.sleep_ms(timOff)
     
-    sole4.value(1)
-    time.sleep_ms(500)
-    sole4.value(0)
-    time.sleep_ms(100)
+    sole4.on()
+    time.sleep_ms(timeOn)
+    sole4.off()
+    time.sleep_ms(timOff)
 
