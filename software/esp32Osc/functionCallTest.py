efe8aa5a8dc8bb607d04c3ce0bfa363c23f23694
jfrazao <jfrazao@gmail.com>
2023-03-30 11:59:51 +0100

    trying to define dispatch function

---
+++ software/esp32Osc/functionCallTest.py
@@ -0,0 +1,57 @@
+# Example 1 - using String?
+# eval("Pin(13,Pin.OUT).value(0)")
+
+# Example 2
+# /function_name argv argc
+# call function_name(argv,argc)
+import network
+
+try:
+    import socket
+except ImportError:
+    import usocket as socket
+
+try:
+    import logging
+except ImportError:
+    import uosc.fakelogging as logging
+
+    
+from uosc.server import handle_osc
+
+    
+log = logging.getLogger("uosc.minimal_server")
+#DEFAULT_ADDRESS = '10.20.23.46'
+#DEFAULT_ADDRESS = '192.168.1.141'
+DEFAULT_ADDRESS = network.WLAN().ifconfig()[0]
+DEFAULT_PORT = 9001
+MAX_DGRAM_SIZE = 1472
+
+# def coustumEasyPinFunction():
+
+def dispatch (timetag, message):
+    print("bla")
+    # from machine import Pin
+    # eval("Pin(13,Pin.OUT)").value(TRUE)
+    # eval("Pin(13,Pin.OUT).value(0)")
+    # eval(message.address)
+    return
+
+def run_server(saddr, port, handler=handle_osc):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    ai = socket.getaddrinfo(saddr, port)[0]
+    sock.bind(ai[-1])
+    log.info("Listening for OSC messages on %s:%i.", saddr, port)
+
+    try:
+        while True:
+            data, caddr = sock.recvfrom(MAX_DGRAM_SIZE)
+            if __debug__: log.debug("RECV %i bytes from %s:",
+                                    len(data),data)
+#            if __debug__: log.debug("RECV %i bytes from %s:%s",
+#                                    len(data), *get_hostport(caddr))
+            handler(data,caddr)
+    finally:
+        sock.close()
+        log.info("Bye!")
