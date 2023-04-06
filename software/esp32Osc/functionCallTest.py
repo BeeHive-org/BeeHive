2800e79bb9c214f3035784891588f87460369fd1
jfrazao <jfrazao@gmail.com>
2023-04-06 10:54:46 +0100

    Remote Procedure Call from bonsai to upython.

--- software/esp32Osc/functionCallTest.py
+++ software/esp32Osc/functionCallTest.py
@@ -18,7 +18,7 @@ except ImportError:
 
     
 from uosc.server import handle_osc
-
+from machine import Pin
     
 log = logging.getLogger("uosc.minimal_server")
 #DEFAULT_ADDRESS = '10.20.23.46'
@@ -27,17 +27,27 @@ DEFAULT_ADDRESS = network.WLAN().ifconfig()[0]
 DEFAULT_PORT = 9001
 MAX_DGRAM_SIZE = 1472
 
-# def coustumEasyPinFunction():
-
-def dispatch (timetag, message):
-    print("bla")
-    # from machine import Pin
-    # eval("Pin(13,Pin.OUT)").value(TRUE)
-    # eval("Pin(13,Pin.OUT).value(0)")
-    # eval(message.address)
+def coustumFunction(pinNumber,pinMode,value):
+    mypin = Pin(pinNumber,pinMode)
+    mypin.value(value)
     return
 
-def run_server(saddr, port, handler=handle_osc):
+def dispatch (timetag, message):
+    #Pin(13,Pin.OUT).value(0)
+    print(timetag)
+    print(message)
+    if (message[0] == '/eval'):
+        eval(message[2][0])
+    #  send back return value of eval
+    elif (message[0] == '/coustumFunction'):
+        coustumFunction(message[2][0],eval(message[2][1]), message[2][2])
+    # send back return value of mycoustum funtion.
+    else:
+        print(message[0][1:])
+        #eval(message[0][1:]+str(message[2]))
+    return
+
+def run_server(saddr = DEFAULT_ADDRESS, port = DEFAULT_PORT, handler=handle_osc):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     ai = socket.getaddrinfo(saddr, port)[0]
@@ -51,7 +61,7 @@ def run_server(saddr, port, handler=handle_osc):
                                     len(data),data)
 #            if __debug__: log.debug("RECV %i bytes from %s:%s",
 #                                    len(data), *get_hostport(caddr))
-            handler(data,caddr)
+            handler(data,caddr,dispatch)
     finally:
         sock.close()
         log.info("Bye!")
